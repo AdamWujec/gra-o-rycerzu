65f1632f27534cd085cd8d2aa7591bab1e2b365d
AdamWujec <Adam.wujec1@o2.pl>
2024-02-20 18:19:56 +0100

    Adam Boss added

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Adamw/CLionProjects/Gra_o_rycerzu
+# Install script for directory: C:/Users/Adamw/OneDrive/Pulpit/GitHub Projekty/gra-o-rycerzu
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Adamw/CLionProjects/Gra_o_rycerzu/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Adamw/OneDrive/Pulpit/GitHub Projekty/gra-o-rycerzu/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
